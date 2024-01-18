34904c3ebd92ab77faafaa2e9300e51fdf2148fb
Salaithomas <88831835+Salai-Thomas@users.noreply.github.com>
2024-01-18 14:47:15 +0630

    Create input_function.py

---
+++ myLearning/input_function.py
@@ -0,0 +1,11 @@
+#input(prompt) [input function (technortal)]
+# prompt is optional ; user ကိုဘယ်လို information input ထည့်ပါဆိုတာပြောချင်တဲ့ခါသုံ:
+# input ကပေးပြန်ပေးလိုက်တဲ့ value သည် အမြဲတမ်းstring type
+
+name = input('Enter Your Name: ')
+print(f'Your Name is {name}')
+age = int(input('Enter Your Age: ')) # int function ဖြင့် integer တန်းပြောင်းလိုက်ခြင်း
+print(f'Your {age} years old!')
+print(type(age))
+
+#float() float data type သို့ပြောင်းလဲခြင်း
