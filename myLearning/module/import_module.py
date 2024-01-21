ea75a06085dff5a9fa24c5e7fa6f921bb274a33b
Salaithomas <88831835+Salai-Thomas@users.noreply.github.com>
2024-01-22 00:12:22 +0630

    Create import_module.py

---
+++ myLearning/module/import_module.py
@@ -0,0 +1,9 @@
+# technortal
+import export_module
+from export_module_2 import export2_var1
+
+print(export_module.export_var_1)
+export_module.export_method()
+
+print(export2_var1)  #သုံးချင်တဲ့ကောင်ကို from နဲ့တစ်ခါတည်း Import လုပ်ရင် သုံးတဲ့အခါ name ကို mention ဖို့မလိုတော့
+
