b02a6a778fa7c12146e9b81364d529c5bb2adcfb
Salaithomas <88831835+Salai-Thomas@users.noreply.github.com>
2024-01-22 09:31:29 +0630

    Create random_module.py

---
+++ myLearning/module/random_module.py
@@ -0,0 +1,12 @@
+#random module from technortal
+import random
+
+random_1_to_10 = random.randint(1,10)
+
+print(random_1_to_10)
+
+lst = ['mg mg','aye aye','hla hla']
+
+random_lst_value = random.choice(lst)
+
+print(random_lst_value)
